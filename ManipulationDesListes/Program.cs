cd2930e81de7298d92d3c93e681a91c76a61b183
K-Dawg92 <kevin.osei.yaw@gmail.com>
2024-06-24 11:52:46 +0200

    Ajoutez des fichiers projet.

---
+++ ManipulationDesListes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ManipulationDesListes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to the StrawHats Crew!");
+
+            List<string> onePieceCharacters = new List<string>();
+            onePieceCharacters.Add("Luffy");
+            onePieceCharacters.Add("Zorro");
+            onePieceCharacters.Add("Sanji");
+            onePieceCharacters.Add("Nami");
+
+            onePieceCharacters.Insert(0, "Jinbei");
+            onePieceCharacters.Insert(3,"Brook");
+            onePieceCharacters.Remove("Nami");
+            onePieceCharacters.RemoveAt(1);
+
+            foreach (string onePieceCharacter in onePieceCharacters) 
+            { Console.WriteLine(onePieceCharacter); }
+        }
+     
+
+        
+    }
+}
